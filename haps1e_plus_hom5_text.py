bcaab50bf29ad8d797477621936efe4b07b42fcf
Haley Abel <abelhj@gmail.com>
2020-02-03 13:37:19 -0600

    uncommented stuff

--- haps1e_plus_hom5_text.py
+++ haps1e_plus_hom5_text.py
@@ -29,54 +29,54 @@ def main():
 
 
 
-  #with gzip.open(args.infile, 'rt') as fp, gzip.open(args.temp_prefix+'.het.txt.gz', 'wt') as fhet, gzip.open(args.temp_prefix+'.mixed.txt.gz', 'wt') as fmix, gzip.open(args.temp_prefix+'.hom.txt.gz', 'wt') as fhom:
-  #  line=fp.readline().strip()
-  #  ct=0
-  #  while line:
-  #    if ct%1000==0:
-  #      sys.stderr.write(str(ct)+'\t'+str(resource.getrusage(resource.RUSAGE_SELF).ru_maxrss/usage_denom)+'\n')
-  #    ll=re.split('[\t]', line)
-  #    [loc1, loc2]=ll[0:2]      
-  #    if not (loc1 in homvar or loc2 in homvar):
-  #      tp='het-het'
-  #      print(line, file=fhet)
-  #    elif not (loc1 in homvar and loc2 in homvar):
-  #      tp='het-hom'
-  #      print(line, file=fmix)
-  #    else:
-  #      tp='hom-hom'
-  #      cts=list(map(int, ll[2:6]))
-  #      mns=list(map(float, ll[6:10]))
-  #      if loc2<loc1:
-  #        [loc1, loc2]=[loc2, loc1]
-  #        cts=[cts[0], cts[2], cts[1], cts[3]]
-  #        mns=[mns[0], mns[2], mns[1], mns[3]]
-  #      [passf, orient, nn, dist]=pre_filter_strict_pass(cts, mns, 0.95, tp)
-  #      if passf:
-  #        print(loc1+'\t'+loc2+'\t'+str(int(dist))+'\t'+orient+'\t'+str(nn), file=fhom)
-  #    line=fp.readline().strip()
-  #    ct+=1
-  #
-  #Ghom=nx.Graph()
-  #ct=0
-  #with gzip.open(args.temp_prefix+'.hom.txt.gz', 'rt') as fp:
-  #  line=fp.readline().strip()
-  #  while line:
-  #    if ct%1000==0:
-  #      sys.stderr.write(str(ct)+'\t'+str(resource.getrusage(resource.RUSAGE_SELF).ru_maxrss/usage_denom)+'\n')
-  #    [loc1, loc2, dist, orient, nn]=re.split('[\t]', line)
-  #    Ghom.add_edge(loc1, loc2, dist=int(dist))
-  #    line=fp.readline().strip()
-  #    ct+=1
-  #
-  #sys.stderr.write('graph in memory\t'+str(resource.getrusage(resource.RUSAGE_SELF).ru_maxrss/usage_denom)+'\n')  
-  #gg=list(Ghom.subgraph(cc) for cc in sorted(nx.connected_components(Ghom), key=len, reverse=True))
-  #sys.stderr.write('comp decomp\t'+str(resource.getrusage(resource.RUSAGE_SELF).ru_maxrss/usage_denom)+'\n')
-  #
-  #with gzip.open(args.temp_prefix+'.comps.hom.txt.gz', 'wt') as fp:
-  #  for ii in range(len(gg)):
-  #    for edge in gg[ii].edges(data=True):
-  #      print(str(ii)+'\t'+edge[0]+'\t'+edge[1]+'\t'+str(edge[2]['dist']), file=fp)
+  with gzip.open(args.infile, 'rt') as fp, gzip.open(args.temp_prefix+'.het.txt.gz', 'wt') as fhet, gzip.open(args.temp_prefix+'.mixed.txt.gz', 'wt') as fmix, gzip.open(args.temp_prefix+'.hom.txt.gz', 'wt') as fhom:
+    line=fp.readline().strip()
+    ct=0
+    while line:
+      if ct%1000==0:
+        sys.stderr.write(str(ct)+'\t'+str(resource.getrusage(resource.RUSAGE_SELF).ru_maxrss/usage_denom)+'\n')
+      ll=re.split('[\t]', line)
+      [loc1, loc2]=ll[0:2]      
+      if not (loc1 in homvar or loc2 in homvar):
+        tp='het-het'
+        print(line, file=fhet)
+      elif not (loc1 in homvar and loc2 in homvar):
+        tp='het-hom'
+        print(line, file=fmix)
+      else:
+        tp='hom-hom'
+        cts=list(map(int, ll[2:6]))
+        mns=list(map(float, ll[6:10]))
+        if loc2<loc1:
+          [loc1, loc2]=[loc2, loc1]
+          cts=[cts[0], cts[2], cts[1], cts[3]]
+          mns=[mns[0], mns[2], mns[1], mns[3]]
+        [passf, orient, nn, dist]=pre_filter_strict_pass(cts, mns, 0.95, tp)
+        if passf:
+          print(loc1+'\t'+loc2+'\t'+str(int(dist))+'\t'+orient+'\t'+str(nn), file=fhom)
+      line=fp.readline().strip()
+      ct+=1
+  
+  Ghom=nx.Graph()
+  ct=0
+  with gzip.open(args.temp_prefix+'.hom.txt.gz', 'rt') as fp:
+    line=fp.readline().strip()
+    while line:
+      if ct%1000==0:
+        sys.stderr.write(str(ct)+'\t'+str(resource.getrusage(resource.RUSAGE_SELF).ru_maxrss/usage_denom)+'\n')
+      [loc1, loc2, dist, orient, nn]=re.split('[\t]', line)
+      Ghom.add_edge(loc1, loc2, dist=int(dist))
+      line=fp.readline().strip()
+      ct+=1
+  
+  sys.stderr.write('graph in memory\t'+str(resource.getrusage(resource.RUSAGE_SELF).ru_maxrss/usage_denom)+'\n')  
+  gg=list(Ghom.subgraph(cc) for cc in sorted(nx.connected_components(Ghom), key=len, reverse=True))
+  sys.stderr.write('comp decomp\t'+str(resource.getrusage(resource.RUSAGE_SELF).ru_maxrss/usage_denom)+'\n')
+  
+  with gzip.open(args.temp_prefix+'.comps.hom.txt.gz', 'wt') as fp:
+    for ii in range(len(gg)):
+      for edge in gg[ii].edges(data=True):
+        print(str(ii)+'\t'+edge[0]+'\t'+edge[1]+'\t'+str(edge[2]['dist']), file=fp)
 
   Ghom=None; homvar=None; gg=None
   oldcomp=-1; treeid=0; ct=0; gg=nx.Graph()
